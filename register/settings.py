ae2f139aed4096967b2ba8e14e0d70e784662aea
Rohit <rohitpradhan240@gmail.com>
2023-05-22 23:01:38 +0530

    update

--- register/settings.py
+++ register/settings.py
@@ -125,9 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATICFILES_DIR = os.path.join(BASE_DIR,'static')
-STATIC_ROOT =   os.path.join(BASE_DIR,'staticfiles_build','static')
 
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
