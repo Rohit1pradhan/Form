15f967a6ea4414735576d03c0d2ef0803a999cd3
Rohit <rohitpradhan240@gmail.com>
2023-05-22 13:33:12 +0530

    initial comit

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from api.models import user
+
+
+# Register your models here.
+@admin.register(user)
+class useradmin(admin.ModelAdmin):
+    list_display = ['id','name','email','phone','dob']
